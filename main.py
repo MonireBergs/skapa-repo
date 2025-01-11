c3f2f03ec1ea55b6b5e9f7db8d73b9c8f791ec42
MonireBergs <monire86@hotmail.com>
2025-01-11 20:31:55 +0100

    Veckouppgift1

--- main.py
+++ main.py
@@ -1,18 +1,4 @@
-# This is a sample Python script.
-
-# Press Skift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+print("Hello world")
+print("This program was made by MonirehBergstrom.")
 
 
-#def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
- #   print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-#if __name__ == '__main__':
- #   print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
-
-print ("Helllo world")
